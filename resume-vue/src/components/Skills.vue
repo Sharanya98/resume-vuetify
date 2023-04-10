424136184c64afa973d66661cdf92a5944a2e503
sbhongade598@gmail.com <sbhongade598@gmail.com>
2023-04-11 04:39:41 +0900

    skills

--- resume-vue/src/components/Skills.vue
+++ resume-vue/src/components/Skills.vue
@@ -50,21 +50,43 @@
 
 
                                     </v-row>
+                                    <v-row class="mb-n10 pr-24">
+                                        <v-col cols="6" md="4">
+                                            <v-label class="label pb-2 font-weight-bold">Sports</v-label>
+                                            <v-combobox class="" v-model="sportsName" :items="skillitems"
+                                                chips  variant="outlined"></v-combobox>
+                                        </v-col>
+                                        <v-col cols="6" md="4">
+                                            <v-label class="label pb-2 font-weight-bold">Years</v-label>
+                                            <v-range-slider v-model="totalYears" strict :max="5" :min="0" thumb-label="always"
+                                                :step="1"></v-range-slider>
+                                        </v-col>
+                                        <v-col cols="6" md="4">
+                                            <v-label class="label pb-2 font-weight-bold block">Add more</v-label>
+                                            <v-btn size="large" color="#5865f2" variant="text" icon="mdi-plus"  @click=""></v-btn>
+                                        </v-col>
+
+
+                                    </v-row>
 
                                     <v-row class="mb-n10">
                                         <v-col cols="4" md="4">
                                             <v-label class="label pb-2 font-weight-bold">Certification</v-label>
                                             <v-combobox v-model="licenseName" :items="licenceitems" chips  variant="outlined"></v-combobox>
                                         </v-col>
-                                        <v-col cols="4" md="4">
+                                        <v-col cols="2" md="2">
                                             <v-label class="label pb-2 font-weight-bold">Date Year</v-label>
                                             <v-text-field v-model="licenseDateYear" variant="outlined" clearable label="YYYY"
                                                 required></v-text-field>
                                         </v-col>
-                                        <v-col cols="4" md="4">
+                                        <v-col cols="2" md="2">
                                             <v-label class="label pb-2 font-weight-bold">Date Month</v-label>
                                             <v-text-field v-model="licenseDateMonth" variant="outlined" clearable label="MM"
                                                 required></v-text-field>
+                                        </v-col>
+                                        <v-col cols="2" md="2">
+                                            <v-label class="label pb-2 font-weight-bold">Add More</v-label>
+                                            <v-btn size="large" color="#5865f2" variant="text" icon="mdi-plus"  @click=""></v-btn>
                                         </v-col>
 
 
